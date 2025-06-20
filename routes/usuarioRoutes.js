231ab10b52df20ba46b9750fc60bab6cc335c32c
Guilherme Santos <97680208+GuiOld@users.noreply.github.com>
2025-06-20 17:47:04 -0300

    Update usuarioRoutes.js

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/usuarioController');
-const auth = require('../middlewares/authMiddleware');
+const auth = require('../middleware/authMiddleware');
 
 router.post('/registrar', controller.registrar);
 router.post('/login', controller.login);
